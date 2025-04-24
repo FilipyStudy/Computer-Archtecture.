879e64d8424776272febee7072a993ba5cdd904b
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2025-04-23 22:10:28 -0300

    Restarted the project

--- exercises/CycleSimulation.c
+++ exercises/CycleSimulation.c
@@ -1,24 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-type_t* MAR;
-type_t MBR;
-type_t PC = fgetc(stdin);
-type_t* IR;
 
-type_t* fetch(PC){  
-    MAR = *PC;
-    PC++;  
-    MBR = &MAR;
-    IR = *MBR;
-    
-    return NULL;
-}
 
-type_t* execute(){
-
-    return NULL;
-}
 int main(int argv, [[maybe_unused]] char* argc[argv + 1]){
 
     return EXIT_SUCCESS;
