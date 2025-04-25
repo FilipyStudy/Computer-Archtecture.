41b5b81ae421024cd873921d9b31577e23eed12e
FilipyStudy <fiilipydasilvafurtado@gmail.com>
2025-04-24 22:42:08 -0300

    Modified and restarted the project

--- exercises/CycleSimulation.c
+++ exercises/CycleSimulation.c
@@ -1,9 +1,43 @@
+##############################################
+#	Computer CPU Cycle Emulator	     #
+#	Made by: Filipy			     #
+#	Offering:		 	     #
+#		Fetch			     #
+#		Decode			     #
+#		Compute			     #
+#		Store			     #
+# Computer Science is a form of art.	     #
+##############################################
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
 
+int firstNumber, secondNumber;
+type_t* programCounter = calloc(1, 8);
+type_t* mAddressRegister = calloc (1, 8);
 
-int main(int argv, [[maybe_unused]] char* argc[argv + 1]){
 
-    return EXIT_SUCCESS;
+type_t* fetch (firstNumber, secondNumber) {
+	return type_t*
+
+}
+
+type_t* decode (int argv, [[maybe_unused]]char* argc[]) {
+	return type_t*
+}
+
+type_t* compute (int argv, [[maybe_unused]]char* argc[]) {
+	return type_t*
+}
+
+type_t* store (int argv, [[maybe_unused]]char* argc[]) {
+	return type_t*
+}
+
+int main (int argv, [[maybe_unused]] char* argc[argv + 1]){
+	scanf("%d", &firstNumber);
+	scanf("%d", &secondNumber);
+	return EXIT_SUCCESS;
 }
